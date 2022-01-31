fe3d41fc4491e66d77138ed3b9c17914911100e7
dani <dani.zaghian@gmail.com>
2022-01-31 11:13:16 -0800

    adds security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
-    articles = HTTP.auth("APIKEYGOESHERE").get("https://newsapi.org/v2/everything?q=#{params[:keyword]}").parse(:json)["articles"]
+    articles = HTTP.auth(Rails.application.credentials.news_api.api_key).get("https://newsapi.org/v2/everything?q=#{params[:keyword]}").parse(:json)["articles"]
     render json: articles
   end
 
